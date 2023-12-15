6284362ea792a6d39426ab7b7934b94b3be0d503
Takaya Funabiki <937552+numa08@users.noreply.github.com>
2023-12-15 22:03:34 +0900

    remove cancellable map provider

--- app/radio_qth_map/lib/widget/operation_map.dart
+++ app/radio_qth_map/lib/widget/operation_map.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:intl/intl.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
@@ -323,7 +322,7 @@ class OperationMapState extends State<OperationMap>
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: "com.covelline.radio_qth_map",
-          tileProvider: CancellableNetworkTileProvider(),
+          tileProvider: NetworkTileProvider(),
         ),
         PolylineLayer(
           polylines: _distanceLines,
