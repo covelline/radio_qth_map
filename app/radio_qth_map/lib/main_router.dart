65792bb853cab37202441c7e8c429f8a5e4ff153
Takaya Funabiki <937552+numa08@users.noreply.github.com>
2024-02-08 14:44:15 +0900

    logging pageview/screenview by firebase analytics

--- app/radio_qth_map/lib/main_router.dart
+++ app/radio_qth_map/lib/main_router.dart
@@ -1,34 +1,37 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:go_router/go_router.dart';
 import 'package:radio_qth_map/screen/add_operation_screen.dart';
 import 'package:radio_qth_map/screen/introduce_screen.dart';
 import 'package:radio_qth_map/screen/map_screen.dart';
 import 'package:radio_qth_map/screen/terms_screen.dart';
+import 'package:radio_qth_map/util/screen_analytics.dart';
 
-final router = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const IntroduceScreen(),
-    ),
-    GoRoute(
-      path: '/map',
-      builder: (context, state) => MapScreen(
-        initialCallsign: state.uri.queryParameters['callsign'],
-      ),
-    ),
-    GoRoute(
-      path: '/map/:id',
-      builder: (context, state) => MapScreen(
-        operationId: state.pathParameters['id'],
-      ),
-    ),
-    GoRoute(
-      path: '/qso/add',
-      builder: (context, state) => const AddOperationScreen(),
-    ),
-    GoRoute(
-      path: '/terms',
-      builder: (context, state) => const TermsScreen(),
-    )
-  ],
-);
+get router => GoRouter(
+      observers: [ScreenAnalytics(analytics: FirebaseAnalytics.instance)],
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const IntroduceScreen(),
+        ),
+        GoRoute(
+          path: '/map',
+          builder: (context, state) => MapScreen(
+            initialCallsign: state.uri.queryParameters['callsign'],
+          ),
+        ),
+        GoRoute(
+          path: '/map/:id',
+          builder: (context, state) => MapScreen(
+            operationId: state.pathParameters['id'],
+          ),
+        ),
+        GoRoute(
+          path: '/qso/add',
+          builder: (context, state) => const AddOperationScreen(),
+        ),
+        GoRoute(
+          path: '/terms',
+          builder: (context, state) => const TermsScreen(),
+        )
+      ],
+    );
